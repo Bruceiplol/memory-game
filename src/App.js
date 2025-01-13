2081c16b9483973f095f2520c9571af9b67a9f43
Bruce Ip <bruceihizzz@gmail.com>
2025-01-13 16:44:15 +0900

    upload files

---
+++ src/App.js
@@ -0,0 +1,163 @@
+import { useState, useEffect } from "react";
+import Form from "./components/Form";
+import MemoryCard from "./components/MemoryCard";
+import AssistiveTechInfo from "./components/AssistiveTechInfo";
+import GameOver from "./components/GameOver";
+import ErrorCard from "./components/ErrorCard";
+
+export default function App() {
+  const initialFormData = { category: "animals-and-nature", number: 10 };
+
+  const [isFirstRender, setIsFirstRender] = useState(true);
+  const [formData, setFormData] = useState(initialFormData);
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [emojisData, setEmojisData] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [areAllCardsMatched, setAreAllCardsMatched] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    if (selectedCards.length === 2) {
+      if (selectedCards[0].name === selectedCards[1].name) {
+        setMatchedCards((prevMatchedCards) => [
+          ...prevMatchedCards,
+          ...selectedCards,
+        ]);
+      } else {
+        setTimeout(() => {
+          setSelectedCards([]);
+        }, 1000);
+      }
+    }
+  }, [selectedCards]);
+
+  useEffect(() => {
+    if (emojisData.length && matchedCards.length === emojisData.length) {
+      setAreAllCardsMatched(true);
+    }
+  }, [matchedCards]);
+
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+
+    for (let i = 0; i < formData.number / 2; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNum)) {
+        randomIndicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+
+    return randomIndicesArray;
+  }
+
+  async function getDataSlice(data) {
+    const randomIndices = getRandomIndices(data);
+
+    const dataSlice = randomIndices.reduce((array, index) => {
+      array.push(data[index]);
+      return array;
+    }, []);
+
+    return dataSlice;
+  }
+
+  async function getEmojisArray(data) {
+    const pairedEmojisArray = [...data, ...data];
+
+    // Fisher-Yates algorithm
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = pairedEmojisArray[i];
+      pairedEmojisArray[i] = pairedEmojisArray[j];
+      pairedEmojisArray[j] = temp;
+    }
+
+    return pairedEmojisArray;
+  }
+
+  function handleFormChange(e) {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  async function startGame(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `https://emojihub.yurace.pro/api/all/category/${formData.category}`
+      );
+
+      if (!res.ok) {
+        throw new Error("Could not fetch data from API");
+      }
+
+      const data = await res.json();
+      const dataSlice = await getDataSlice(data);
+      const emojisArray = await getEmojisArray(dataSlice);
+
+      setEmojisData(emojisArray);
+      setIsGameOn(true);
+    } catch (err) {
+      console.error(err);
+      setIsError(true);
+    } finally {
+      setIsFirstRender(false);
+    }
+  }
+
+  function turnCard(name, index) {
+    if (selectedCards.length < 2) {
+      setSelectedCards((prevSelectedCards) => [
+        ...prevSelectedCards,
+        { name, index },
+      ]);
+    } else if (selectedCards.length === 2) {
+      setSelectedCards([{ name, index }]);
+    }
+  }
+
+  function resetGame() {
+    setIsGameOn(false);
+    setSelectedCards([]);
+    setMatchedCards([]);
+    setAreAllCardsMatched(false);
+  }
+
+  function resetError() {
+    setIsError(false);
+  }
+
+  return (
+    <main>
+      <h1>Memory</h1>
+      {!isGameOn && !isError && (
+        <Form
+          handleSubmit={startGame}
+          handleChange={handleFormChange}
+          isFirstRender={isFirstRender}
+        />
+      )}
+      {isGameOn && !areAllCardsMatched && (
+        <AssistiveTechInfo
+          emojisData={emojisData}
+          matchedCards={matchedCards}
+        />
+      )}
+      {isGameOn && (
+        <MemoryCard
+          handleClick={turnCard}
+          data={emojisData}
+          selectedCards={selectedCards}
+          matchedCards={matchedCards}
+        />
+      )}
+      {areAllCardsMatched && <GameOver handleClick={resetGame} />}
+      {isError && <ErrorCard handleClick={resetError} />}
+    </main>
+  );
+}
